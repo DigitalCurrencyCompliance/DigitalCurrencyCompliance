717318329c422fbbf40dc41f0fcfc194a7939072
Aaron Anderson <aaronlynanderson@gmail.com>
2018-09-08 14:57:19 -0600

    app stores data in database

--- controllers/account.controller.js
+++ controllers/account.controller.js
@@ -39,11 +39,29 @@ exports.infura = async function ( req, res ) {
   res.send (response);
 }
 
+exports.new_account = async function ( req, res ) {
+  let newAccount = new Account (
+    {
+      ein: '123456-78',
+      password: 'password',
+      companyType: 'LLC',
+      BTC: 'BtcAddress',
+      ETH: 'EthAddress',
+      PIVX: 'PivxAddress',
+      DCT: 'DctAddress',
+      DAI: 'DaiAddress'
+    }
+  );
 
-
-// exports.test = function ( req, res ) {
-//     res.send ( 'Greetings from the account Test controller!' );
-// };
+  //save account object to the database
+  newAccount.save ( function ( err, dbResponse ) {
+    if ( err ) {
+      res.send( err );
+    }
+    console.log ( "***" + chalk.white( dbResponse ) + "***" );
+    res.send ( dbResponse );
+  });
+}
 
 // // vvv create method *** This is where the magic happens ***********************
 // exports.create_transaction = async function ( req, res ) {
--- routes/account.route.js
+++ routes/account.route.js
@@ -8,7 +8,7 @@ const account_controller = require('../controllers/account.controller');
 router.get('/test', account_controller.test);
 
 // create a new accounts
-router.get('/newAccount', account_controller.test);
+router.get('/newAccount', account_controller.new_account);
 
 // create a BTC wallet
 router.post('/newBtcWallet', account_controller.test);
@@ -19,8 +19,8 @@ router.post('/newEthWallet', account_controller.test);
 // this GETs the infura api Ethereum Block Data
 router.get('/infura', account_controller.infura);
 
-// creat a DECENT walletAddress
-router.post('./newDecentWallet', account_controller.test);
+// creat a DCT walletAddress
+router.post('./newDctWallet', account_controller.test);
 
 // create a PIVX walletAddress
 router.post('./newPivxWallet', account_controller.test);
