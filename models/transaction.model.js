b0461dcdd447b208c04a0fd50f532a895196a511
Aaron Anderson <aaronlynanderson@gmail.com>
2018-09-08 01:56:26 -0600

    adds routing for transactions

---
+++ models/transaction.model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let TransactionSchema = new Schema({
+    currency: {type: String, required: false, max: 64},
+    amount: {type: Number, required: false},
+    transactionId: {type: String, required: false, max: 64},
+    walletAddress: {type: String, required: false, max: 64},
+    status: {type: String, required: true, default: "pending", max: 32},
+    productId: {type: String, required: false, max: 64},
+    unconfirmed: {type: Number, required: false, max: 64},
+    mined: {type: Boolean, required: false},
+    recordBlock: {type: String, required: false, max: 64},
+    rawTx: {type: String, required: false}
+});
+
+
+// Export the model
+module.exports = mongoose.model('Transaction', TransactionSchema);
