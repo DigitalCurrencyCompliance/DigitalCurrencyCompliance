9e4ed0448ab49e8941b5f0c17339549e4647ccd1
insideoutzombie <david@web3devs.com>
2018-09-08 01:41:35 -0500

    add create newWallet

--- routes/account.route.js
+++ routes/account.route.js
@@ -7,6 +7,9 @@ const account_controller = require('../controllers/account.controller');
 // a simple test url to check that all of our files are communicating correctly.
 router.get('/test', account_controller.test);
 
+// create a wallet
+router.post('/newWallet', account_controller.test);
+
 // // create a transaction.
 // router.post('/dcc/createTransaction', account_controller.create_transaction);
 //
