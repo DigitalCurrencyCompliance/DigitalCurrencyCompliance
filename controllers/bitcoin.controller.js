d54a1b5b89f66196c4ee0002f41f618007dc8c3a
Aaron Anderson <aaronlynanderson@gmail.com>
2018-09-08 23:21:09 -0600

    updates the account model for all addresses to have a length of 64

--- controllers/bitcoin.controller.js
+++ controllers/bitcoin.controller.js
@@ -1,14 +1,36 @@
 const chalk = require ( 'chalk' );
-const request = require ('superagent');
+const request = require ('superagent' );
+const bitcore = require ( 'bitcore-lib' )
 // const bodyParser = require ('body-parser');
 let home = "http://127.0.0.1:";
-let rpcport = "51473"; // JSONRPC server handles only POST requests
-let testnet = "51474";
-let mainnet = "51472";
+// let rpcport = "51473"; // JSONRPC server handles only POST requests
+// let testnet = "51474";
+// let mainnet = "51472";
 const credentials = require ( '../credentials' )
 let btcData = {};
 
 exports.get_new_address = async function (req,res){
+      // create new bitcoin address
+      let rand_buffer = bitcore.crypto.Random.getRandomBuffer(32);
+      let rand_number = bitcore.crypto.BN.fromBuffer(rand_buffer);
+      let rand_string = rand_number.toString();
+
+      let pKey = credentials.btcPrivKey;
+      let privateKey = bitcore.PrivateKey.fromWIF(pKey);
+      let address = privateKey.toAddress();
+      console.log(chalk.white(address));
+
+      let value = new Buffer(rand_string);
+      let hash = bitcore.crypto.Hash.sha256(value);
+      let bn = bitcore.crypto.BN.fromBuffer(hash);
+      // let newAddress = new bitcore.PrivateKey(bn, 'testnet').toAddress();
+      // let newAddress = bitcore.getNewAddress(privateKey);
+
+      // console.log("newAddress:");
+      // console.log(typeof newAddress);
+      // console.log(newAddress);
+      // newAddress =
+      // console.log(newAddress[0]);
     // try {
     //     await request
     //           .post (home + rpcport)
@@ -29,7 +51,7 @@ exports.get_new_address = async function (req,res){
     //       console.log ( chalk.red ( "REQUEST ERROR in get_new_address" ) );
     //       return pivxData.result;
     //   };
-      btcData.result = "controller fired"
+      btcData.result = pKey;
       return btcData.result;
 }
 
